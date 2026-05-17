16a3c9f421535fc2c86d7094398161fc280a2f00
michelangeloTricarico <michelangelotricarico@gmail.com>
2026-05-17 18:35:56 +0200

    Work finalization

--- src/pages/AppContactsPage.jsx
+++ src/pages/AppContactsPage.jsx
@@ -1,5 +1,5 @@
 export default function ContactsPage(){
     return(
-        <div>Chi siamo</div>
+        <div className="banner-text text-black fs-3 m-5">MyStore è uno shop online nato con l’obiettivo di offrire prodotti selezionati di qualità a prezzi competitivi. Crediamo in un’esperienza d’acquisto semplice, veloce e trasparente, mettendo sempre al centro il cliente e le sue esigenze. Ogni prodotto presente nel nostro catalogo viene scelto con cura per garantire affidabilità, stile e convenienza.</div>
     )
 }
