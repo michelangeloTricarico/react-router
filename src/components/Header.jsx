abe486062720c30f56a1a25ab43a29da2a9ca100
michelangeloTricarico <michelangelotricarico@gmail.com>
2026-05-23 17:46:55 +0200

    Added budgetMode and BudgetValue

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,12 @@
 import { Link, NavLink } from "react-router"
+import BudgetContext from "../context/BudgetContext"
+import { useState } from "react"
 export default function Header({NavList}){
-    return(
+    const [budgetMode,setBudgetMode] = useState(false)
+    const [budgetValue,setBudgetValue] = useState("")
+    return( 	
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <BudgetContext.Provider value={{budgetMode, budgetValue}}/>
             <div className="container-fluid">
                 <a className="navbar-brand" href="#">MyShop</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -15,6 +20,16 @@ export default function Header({NavList}){
                         </li>
                     ))}
                 </ul>
+                <form className="d-flex ms-auto" role="filter">
+                <input onChange={(e)=>{
+                    if(!isNaN(e.target.value)){
+                        setBudgetValue(e.target.value)
+                    } 
+                    }} value={budgetValue} className="form-control me-2" type="filter" placeholder="Budget value" aria-label="Search"/>
+                </form>
+                <div className="form-check">
+                    <input onChange={(e)=>{setBudgetMode(e.target.checked)}} className="form-check-input" type="checkbox" value={budgetMode} id="checkDefault"/>
+                </div>
                 </div>
             </div>
             </nav>
